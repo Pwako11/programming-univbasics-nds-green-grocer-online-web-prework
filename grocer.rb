6588f301a6d0437dd9171198269e51245eab2650
Patrick A <akolo@hotmail.com>
2020-03-01 16:47:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,6 +5,12 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   
+  index = 0 
+  
+  while index < collection.length do 
+    
+  end
+  
   binding.pry
   
 end
